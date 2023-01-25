f5c2865c1399c02113c1a78df8658b6b76b79d0f
egesenli <egesenli@gmail.com>
2023-01-25 18:05:46 -0500

    Implement some SQL injection in our application

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,17 +11,20 @@ const pool = new Pool({
 
 //Querying the database
 
-const cohortName = process.argv[2];
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
       console.log(`${cohortName}: ${teacher.teacher}`);
